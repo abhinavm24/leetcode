8cc0956f61a77fa63d8b99ea7c30d10fddb6201c
Abhinav Misra <mrabhinavmisra@gmail.com>
2020-08-05 10:03:12 +0530

    update problem 1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -3,13 +3,39 @@
 #
 # [1] Two Sum
 #
+# https://leetcode.com/problems/two-sum/description/
+#
+# algorithms
+# Easy (45.59%)
+# Likes:    16018
+# Dislikes: 581
+# Total Accepted:    3.1M
+# Total Submissions: 6.9M
+# Testcase Example:  '[2,7,11,15]\n9'
+#
+# Given an array of integers, return indices of the two numbers such that they
+# add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may
+# not use the same element twice.
+#
+# Example:
+#
+#
+# Given nums = [2, 7, 11, 15], target = 9,
+#
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+#
+#
+#
 
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """[summary]
-        Maintain dictionary/hash map of complementary values of seen nums
-        Check for num availability in dictionary to avoid two passes
+        Maintain a dictionary with complementary num as key and index as value
+        Check if number available in dict, to avoid two runs over list
         Return empty list if no solution
 
         Args:
@@ -24,9 +50,7 @@ class Solution:
         for i, num in enumerate(nums):
             if num in seen:
                 return [seen[num], i]
-            else:
-                seen[target - num] = i
+            seen[target - num] = i
         return []
-
 
 # @lc code=end
