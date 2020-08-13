55e14770771124a2c1ebd0e51fe74917532c92c0
Abhinav Misra <mrabhinavmisra@gmail.com>
2020-08-13 13:43:08 +0530

    fibonacci solved

---
+++ 509.fibonacci-number.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+# https://leetcode.com/problems/fibonacci-number/description/
+#
+# algorithms
+# Easy (67.17%)
+# Likes:    686
+# Dislikes: 193
+# Total Accepted:    222.4K
+# Total Submissions: 331.1K
+# Testcase Example:  '2'
+#
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the
+# Fibonacci sequence, such that each number is the sum of the two preceding
+# ones, starting from 0 and 1. That is,
+#
+#
+# F(0) = 0,   F(1) = 1
+# F(N) = F(N - 1) + F(N - 2), for N > 1.
+#
+#
+# Given N, calculate F(N).
+#
+#
+#
+# Example 1:
+#
+#
+# Input: 2
+# Output: 1
+# Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+#
+#
+# Example 2:
+#
+#
+# Input: 3
+# Output: 2
+# Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+#
+#
+# Example 3:
+#
+#
+# Input: 4
+# Output: 3
+# Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+#
+#
+#
+#
+# Note:
+#
+# 0 ≤ N ≤ 30.
+#
+#
+
+# @lc code=start
+from functools import lru_cache
+
+
+class Solution:
+    @lru_cache(maxsize=31)
+    def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        else:
+            return self.fib(N-1) + self.fib(N-2)
+
+# @lc code=end
